2f72f3f21d50d8197f056d17ec63ef6065e0079d
ys0uyno <ys0uyno@163.com>
2019-01-23 10:29:19 +0800

    how to call SendMessage in Qt Creator

--- non_qt/main.cpp
+++ non_qt/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <Windows.h>
 
-using namespace std;
+#define WM_QT_MSG_0 WM_USER + 1001
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    HWND hwnd = FindWindowA(nullptr, "qt_msg");
+    if (hwnd) {
+        SendMessageA(hwnd, WM_QT_MSG_0, 0, 0);
+    }
+    else {
+        std::cout << "cannot find window qt_msg" << std::endl;
+    }
+
     return 0;
 }
--- non_qt/non_qt.pro
+++ non_qt/non_qt.pro
@@ -3,5 +3,7 @@ CONFIG += console c++11
 CONFIG -= app_bundle
 CONFIG -= qt
 
+LIBS += -luser32
+
 SOURCES += \
         main.cpp
